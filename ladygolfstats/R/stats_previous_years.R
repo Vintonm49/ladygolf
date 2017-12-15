26991f33ee120dcb7b1a395fc00bd456ce2da86c
Vintonm49 <usmafi@yahoo.com>
2017-12-15 13:13:53 -0500

    Add new function

--- ladygolfstats/R/stats_previous_years.R
+++ ladygolfstats/R/stats_previous_years.R
@@ -12,14 +12,39 @@ lpga_getlinks <- function(year = 2016){
   links <- front_page %>%
     rvest::html_nodes('a.button') %>%
     rvest::html_attr('href')
-  
+
   # keep just the links that go to a statistics page
   statIndex <- grep("statistics", links)
   links <- links[statIndex]
-  
+
   # add the front part of the URL
   links <- paste0("http://www.lpga.com", links)
   links16 <- paste0(links,"?year=",year)
   return(links16)
-  
+
 }
+
+
+#' Scrape LPGA Stats Pages
+#'
+#' This function takes a list of website pages from LPGA.com for current year or prior years statistics
+#' and pulls the data table. The list can be the output from the lpga_getlinks() function.
+#'  A data frame is created from the table from each page  and added to a list of data frames.
+#'  Output is a list of dataframes.
+#'
+#' @param links  A list of website URLs
+#' @export
+
+lpga_scrape <- function(links = NULL){
+listofdfs <- list()
+for(i in 1:length(links)){
+  page <- read_html(links[i])
+  table <- page %>%
+    html_node('table.table') %>%
+    html_table()
+  listofdfs[[i]]<- table
+}
+return(listofdfs)
+}
+
+
